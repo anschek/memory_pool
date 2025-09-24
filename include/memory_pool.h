b1c6542014bf8116ac5ed8f1bf59f2347d69a34e
anschek <anschek@yandex.ru>
2025-09-24 13:45:48 +0300

    feat: initialize pool, allocate memory block

--- include/memory_pool.h
+++ include/memory_pool.h
@@ -4,5 +4,15 @@
 
 #ifndef MEMORY_POOL_H
 #define MEMORY_POOL_H
+#include <iostream>
+struct MemoryPool {
+    __int8* memory;
+    size_t total_size;
+    size_t block_size;
+    size_t num_blocks;
+    bool* used; // массив флагов: занят ли блок
+};
 
+MemoryPool* init_pool(size_t total_bytes, size_t block_size);
+void* allocate(MemoryPool* pool);
 #endif //MEMORY_POOL_H
--- src/memory_pool.cpp
+++ src/memory_pool.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "../include/memory_pool.h"
+MemoryPool* init_pool(size_t total_bytes, size_t block_size) {
+    __int8* pool = new __int8[total_bytes];
+    size_t num_blocks = total_bytes / block_size;
+    bool* used = new bool[num_blocks]{}; // по умолчанию везде false (0)
+
+    return new MemoryPool(pool, total_bytes, block_size, num_blocks, used);
+}
+
+// Возвращает указатель на первый свободный блок
+void* allocate(MemoryPool* pool) {
+    for (size_t i = 0; i < pool->num_blocks; ++i)
+        if (!pool->used[i]) {
+            pool->used[i] = true;
+            return pool->memory + i * pool->block_size;
+        }
+    return nullptr;
+}
