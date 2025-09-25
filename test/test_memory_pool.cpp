60e0879332d6a2eb4a8384cff6096498d15f19ad
anschek <anschek@yandex.ru>
2025-09-25 13:17:25 +0300

    test: check pool reset and destroy

--- test/test_memory_pool.cpp
+++ test/test_memory_pool.cpp
@@ -2,21 +2,20 @@
 // Created by anschek on 25.09.2025.
 //
 
-#include <cassert>
-
+#include "tests.h"
 #include "../include/memory_pool.h"
+
+#include <cassert>
+#include <iostream>
 
 void test_init(MemoryPool *&pool) {
     size_t total_bytes = 6, block_size = 2;
     pool = init_pool(total_bytes, block_size);
-    std::cout << pool << std::endl;
     assert(pool != nullptr);
     assert(pool->total_size == total_bytes);
     assert(pool->block_size == block_size);
 
-    for (size_t i = 0; i < pool->num_blocks; ++i)
-        std::cout << pool->used[i] << ' ';
-    std::cout << '\n' << __func__ << " passed\n";
+    std::cout << '\t' << __func__ << " passed\n";
 }
 
 void test_allocate(MemoryPool *pool) {
@@ -34,7 +33,7 @@ void test_allocate(MemoryPool *pool) {
     void *memory4 = allocate(pool);
     assert(memory4 == nullptr);
 
-    std::cout << __func__ << " passed\n";
+    std::cout << '\t' << __func__ << " passed\n";
 }
 
 void test_deallocate(MemoryPool *pool) {
@@ -45,7 +44,7 @@ void test_deallocate(MemoryPool *pool) {
 
     // ошибка выхода за границы пула
     try {
-        __int8 *memory4 = static_cast<__int8 *>(memory1) + 4*2;
+        char *memory4 = static_cast<char *>(memory1) + 4*2;
         deallocate(pool, memory4);
         assert(false);
     } catch (const std::exception &e) {
@@ -54,7 +53,7 @@ void test_deallocate(MemoryPool *pool) {
 
     // ошибка выравнивания блока памяти
     try {
-        __int8 *memory3 = static_cast<__int8 *>(memory1) + 3;
+        char *memory3 = static_cast<char *>(memory1) + 3;
         deallocate(pool, memory3);
         assert(false);
     } catch (const std::exception &e) {
@@ -69,13 +68,48 @@ void test_deallocate(MemoryPool *pool) {
         std::cout << e.what() << '\n';
     }
 
-    std::cout << __func__ << " passed\n";
+    std::cout << '\t' << __func__ << " passed\n";
+}
+
+void test_reset(MemoryPool *pool) {
+    reset_pool(pool);
+    for (size_t i = 0; i < pool->num_blocks; ++i)
+        assert(!pool->used[i]);
+
+    // освобождение свободного блока
+    try {
+        deallocate(pool, pool->memory);
+        assert(false);
+    }catch (const std::exception &e) {
+        std::cout << e.what() << '\n';
+    }
+
+    // переиспользование
+    void* memory1 = allocate(pool);
+    assert(memory1 != nullptr);
+    assert(pool->used[0]);
+
+    std::cout << '\t' << __func__ << " passed\n";
+}
+
+void test_free(MemoryPool *&pool) {
+    destroy_pool(pool);
+    assert(!pool);
+
+    std::cout << '\t' << __func__ << " passed\n";
 }
 
 void memory_pool_test_all() {
     MemoryPool *pool = nullptr;
     test_init(pool);
+    assert(pool);
+
     test_allocate(pool);
     test_deallocate(pool);
-    std::cout << "all tests passed\n";
+    test_reset(pool);
+
+    test_free(pool);
+    assert(!pool);
+
+    std::cout << '\t' << __func__ << " passed\n";
 }
